abfb0793a1602adb3380f50b2c0148fe562dac23
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:35:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "iotdevicemanagement-45943",
+  url: "https://iotdevicemanagement-45943.botics.co",
+  api: "https://iotdevicemanagement-45943.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
